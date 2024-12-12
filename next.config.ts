1ea7df615c49849438e2fc77a1ebe4741ad53908
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2024-12-12 19:28:20 +0500

    uppdated next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreBuildErrors: true,
-  images: {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+    images: {
     remotePatterns: [
       {
         protocol: "https",
